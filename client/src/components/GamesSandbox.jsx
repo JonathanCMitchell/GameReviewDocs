2a365f1bc9b9e3ddaf250aa1fbfdf43812918cb4
= <Jmitchell1991@gmail.com>
2016-10-13 17:11:46 -0700

    [feature] - adds form validation for editors choice

--- client/src/components/GamesSandbox.jsx
+++ client/src/components/GamesSandbox.jsx
@@ -1,42 +1,83 @@
 import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
+import Divider from 'material-ui/Divider';
 
 class GamesSandbox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: '',
+      title: '',
       score_range: '',
-      errorFirstName: '',
+      score: '',
+      release_year: '',
+      errorTitle: '',
       errorScoreRange: '',
+      errorScore: '',
+      errorReleaseYear: '',
     };
-    this.onFirstNameChange = this.onFirstNameChange.bind(this);
+    this.onTitleChange = this.onTitleChange.bind(this);
     this.onScoreRangeChange = this.onScoreRangeChange.bind(this);
+    this.onScoreChange = this.onScoreChange.bind(this)
+    this.onEditorsChoiceChange = this.onEditorsChoiceChange.bind(this);
   }
-  onFirstNameChange(event) {
-    const regex = /^[a-zA-Z]{1,50}$/; // limits string length, min 1, max 50, letters only
+  onTitleChange(event) {
+    const regex = /^[A-Z]/; // limits string length, min 1, max 50, letters only
     this.setState({
-      firstName: event.target.value
+      title: event.target.value,
     });
 
     if (regex.test(event.target.value)) {
-      this.setState({ errorFirstName: '' });
+      this.setState({ errorTitle: '' });
     }
     else {
-      this.setState({ errorFirstName: 'Invalid input' });
+      this.setState({ errorTitle: 'Invalid title' });
     }
   }
 
   onScoreRangeChange(event) {
     const regex = /^[0-9], ([1-9]|10)$/g
     this.setState({
-      score_range: event.target.value
+      score_range: event.target.value,
     });
-
     if (regex.test(event.target.value)) {
       this.setState({ errorScoreRange: ''});
     } else {
       this.setState({ errorScoreRange: 'Invalid score range'});
+    }
+  }
+
+  onScoreChange(event) {
+    const regex = /^([1-9]|10)$/g;
+    this.setState({
+      score: event.target.value,
+    });
+    if (regex.test(event.target.value)) {
+      this.setState({ errorScore: ''});
+    } else {
+      this.setState({ errorScore: 'Invalid score'});
+    }
+  }
+
+  onEditorsChoiceChange(event) {
+    const regex = /^1|0$/g;
+    this.setState({
+      editors_choice: event.target.value
+    });
+    if (regex.test(event.target.value)) {
+      this.setState({ errorEditorsChoice: '' });
+    } else {
+      this.setState({ errorEditorsChoice: 'Invalid editors choice parameter'});
+    }
+  }
+  onReleaseYearChange(event) {
+    const regex = /^(199\d|200\d|2010)$/
+    this.setState({
+      release_year: event.target.value,
+    });
+    if (regex.test(event.target.value)) {
+      this.setState({ errorReleaseYear: '' });
+    } else {
+      this.setState({ errorReleaseYear: 'Invalid release year parameter'});
     }
   }
 
@@ -55,15 +96,46 @@ class GamesSandbox extends Component {
             errorText={this.state.errorScoreRange}
             underlineShow={false}
           />
-        <TextField
-            floatingLabelText="first name"
-            className="auth-input-text"
-            onChange={this.onFirstNameChange}
-            value={this.state.firstName}
-            hintText="First Name"
-            errorText={this.state.errorFirstName}
+          <Divider />
+          <TextField
+            floatingLabelText="score"
+            className="score-input-text"
+            onChange={this.onScoreChange}
+            value={this.state.score}
+            hintText="score"
+            errorText={this.state.errorScore}
             underlineShow={false}
           />
+          <Divider />
+          <TextField
+            floatingLabelText="editors_choice"
+            className="editors_choice-input-text"
+            onChange={this.onEditorsChoiceChange}
+            value={this.state.editors_choice}
+            hintText="editors choice"
+            errorText={this.state.errorEditorsChoice}
+            underlineShow={false}
+          />
+          <Divider />
+        <TextField
+            floatingLabelText="title"
+            className="title-input-text"
+            onChange={this.onTitleChange}
+            value={this.state.title}
+            hintText="Title"
+            errorText={this.state.errorTitle}
+            underlineShow={false}
+          />
+          <TextField
+            floatingLabelText="title"
+            className="title-input-text"
+            onChange={this.onTitleChange}
+            value={this.state.title}
+            hintText="Title"
+            errorText={this.state.errorTitle}
+            underlineShow={false}
+          />
+         
       </form>
       <h1>Hello we are inside GamesSandbox</h1>
       <p>This will be received data sandbox</p>
